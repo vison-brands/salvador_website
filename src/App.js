7d6abddac634c88668fdece671b64994fbd24002
wilcastiblanco <wcastiblanco@outlook.com>
2022-05-31 22:46:10 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import profile from './salvadorContainer/home/profile';
+function App() {
+  return (
+    <div className="App"> 
+      <profile />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/salvadorContainer/home/profile.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function profile() {
+  return (
+    <div className='profile-container'>
+        <div className='profile-parent'>
+            <div className='profile-details'>
+                <div className='colz'>
+                    <ul>
+                        <li>Home</li>
+                        <li>Bio</li>
+                        <li>Music</li>
+                        <li>Dates</li>
+                        <li>Shop</li>
+                        <li>Newsletter</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
